d5da9bc3f971012ec695ac32a6132a254692d311
chaochao0728 <907196317@qq.com>
2016-10-17 19:11:20 +0800

    写了Fragment的基类

--- MaoYan/app/src/main/java/com/yangyuning/maoyan/base/A.java
+++
@@ -1,7 +0,0 @@
-package com.yangyuning.maoyan.base;
-
-/**
- * Created by dllo on 16/10/17.
- */
-public class A {
-}
---
+++ MaoYan/app/src/main/java/com/yangyuning/maoyan/base/AbsBaseFragment.java
@@ -0,0 +1,57 @@
+package com.yangyuning.maoyan.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dllo on 16/10/17.
+ * Fragment的基类
+ *
+ * @author 韩朝
+ */
+public abstract class AbsBaseFragment extends Fragment {
+    protected Context context;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(setLayout(), container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initDatas();
+    }
+
+    protected abstract int setLayout();
+
+    protected abstract void initView();
+
+    protected abstract void initDatas();
+
+    //简化 findViewById
+    protected <T extends View> T byView(int resId) {
+        return (T) getView().findViewById(resId);
+    }
+
+
+}

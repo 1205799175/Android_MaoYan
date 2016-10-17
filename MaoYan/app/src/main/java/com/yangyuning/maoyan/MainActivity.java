ceb493a59cdb5c5ca0f281b35a071689dc61d8cb
1205799175 <1205799175@qq.com>
2016-10-17 17:23:59 +0800

    创建工程

---
+++ MaoYan/app/src/main/java/com/yangyuning/maoyan/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yangyuning.maoyan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

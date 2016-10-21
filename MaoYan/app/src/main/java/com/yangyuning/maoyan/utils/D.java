c8b407969b0848d03d8dfbc774ab5473fe46eaa5
1205799175 <1205799175@qq.com>
2016-10-21 19:42:44 +0800

    杨宇宁:修改项目, 暂时未加功能

--- MaoYan/app/src/main/java/com/yangyuning/maoyan/utils/D.java
+++
@@ -1,7 +0,0 @@
-package com.yangyuning.maoyan.utils;
-
-/**
- * Created by dllo on 16/10/17.
- */
-public class D {
-}
---
+++ MaoYan/app/src/main/java/com/yangyuning/maoyan/utils/MaoYanValue.java
@@ -0,0 +1,10 @@
+package com.yangyuning.maoyan.utils;
+
+/**
+ * Created by dllo on 16/10/21.
+ * 常量类
+ */
+public class MaoYanValue {
+    public MaoYanValue(){}
+
+}
